d193d61f09c4c23be70af651c3e86ddde04aac20
Dmytro Kotykhin <kotykhin_d@ukr.net>
2023-06-28 12:59:40 +0200

    import bug fixed

--- src/components/authForms/SetPasswordForm.tsx
+++ src/components/authForms/SetPasswordForm.tsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify';
 
 import { Button, Container, Typography, Avatar, Paper, Box } from "@mui/material";
 
-import PasswordField from './fields/PasswordField';
+import { PasswordField } from './fields/_index';
 import { NewPasswordFormValidation } from '../validation/userFormValidation';
 
 import { useFetchSetPasswordMutation } from 'services/userService';
